30796f73f6328cdfc3e3af2311e27497d2f69cf0
Aya Israeli <ayaisraeli12@gmail.com>
2025-11-26 23:37:06 +0200

    started BP_predict

--- CompArch-hw1/bp.cpp
+++ CompArch-hw1/bp.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 struct line
 {
-	int tag;
-	int target;
+	uint32_t tag;
+	uint32_t target;
 	int history_index;
 };
 
@@ -79,13 +79,36 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned f
 bool BP_predict(uint32_t pc, uint32_t *dst)
 {
 	// search tag in BTB
-		// command in btb:
+	for (int i = 0; i < BTB_context.BTB.size(); i++)
+	{
+		if (BTB_context.BTB[i].tag == get_relevant_tag(pc)) // assuming not relevant bits of "tag" are also 0
+		{
+			// command in btb:
 			// go to history -> go to relevant state machine -> get prediction
-				// predicted Taken: return true, target
-				// predicted NotTaken: return false
+			// predicted Taken: return true, target
+			// predicted NotTaken: return false
 
-	// command not in btb: return false
-	return false;
+			int const not_using_share = 0;
+			int const using_share_lsb = 1;
+			int const using_share_mid = 2;
+
+			switch (BTB_context.Shared) // history -> do xors accordingly -> relevant state machine
+			{
+			case not_using_share:
+				break;
+			case using_share_lsb:
+				break;
+			case using_share_mid:
+				break;
+			default:
+				break;
+			}
+		}
+		
+		// command not in btb: return false
+
+		return false;
+	}
 }
 
 void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst)
@@ -97,4 +120,17 @@ void BP_GetStats(SIM_stats *curStats)
 {
 	// delete BTB_context
 	return;
+}
+
+// can use get_relevant_tag when entering new line to BTB (to get the new tag)
+uint32_t get_relevant_tag(uint32_t pc)
+{
+	// pc has 2 bits of 0, then log2(btbSize) bits for entry, then tagSize bits for tag
+	int tag_start = 2 + log2(BTB_context.btbSize);
+	int tag_finish = tag_start + BTB_context.tagSize - 1;
+	// create a mask with 0 where pc bits are not for tag,
+	// and 1 on bits for the tag:
+	// (0u = 32 bits of 0, ~0u = 32 bits of 1)
+	uint32_t mask = ((~0u) >> tag_start) & ((~0u) << tag_finish);
+	return pc & mask;
 }
