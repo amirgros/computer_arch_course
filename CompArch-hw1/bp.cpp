90a974562c2fc889abfbcbb701919558cb40dc1a
Aya Israeli <ayaisraeli12@gmail.com>
2025-11-30 19:40:49 +0200

    fixed warnings & flushes counter

--- CompArch-hw1/bp.cpp
+++ CompArch-hw1/bp.cpp
@@ -7,7 +7,6 @@
 
 using namespace std;
 
-
 // #########################################
 // Structs, Globals and Macros
 // ##########################################
@@ -63,8 +62,7 @@ uint32_t get_relevant_tag(uint32_t pc)
 int get_btb_index(uint32_t pc)
 {
 	// return btb index from pc (y in {zzzzzzzzzz yyyy 00})
-	uint32_t bits_in_index = log2(BTB_context.btbSize);
-	uint32_t mask = BTB_context.btbSize - 1; 
+	uint32_t mask = BTB_context.btbSize - 1;
 	return (pc >> 2) & mask;
 }
 
@@ -80,23 +78,23 @@ int fsm_index(uint32_t pc, int history_index)
 	// return the index of relevant row in FSM table
 	int hist_val = BTB_context.history[history_index];
 	int fsm_ind = hist_val;
-		if (BTB_context.isGlobalTable) // L\Gshare relevant
+	if (BTB_context.isGlobalTable) // L\Gshare relevant
+	{
+		uint32_t mask = (1 << BTB_context.historySize) - 1;
+		switch (BTB_context.Shared)
 		{
-			uint32_t mask = (1 << BTB_context.historySize) - 1;
-			switch (BTB_context.Shared)
-			{
-			case NOT_USING_SHARE:
-				break;
-			case USING_SHARE_LSB:
-				fsm_ind = (mask & (pc >> 2)) ^ hist_val;
-				break;
-			case USING_SHARE_MID:
-				fsm_ind = (mask & (pc >> 16)) ^ hist_val;
-				break;
-			default:
-				break;
-			}
+		case NOT_USING_SHARE:
+			break;
+		case USING_SHARE_LSB:
+			fsm_ind = (mask & (pc >> 2)) ^ hist_val;
+			break;
+		case USING_SHARE_MID:
+			fsm_ind = (mask & (pc >> 16)) ^ hist_val;
+			break;
+		default:
+			break;
 		}
+	}
 	return fsm_ind;
 }
 
@@ -115,11 +113,11 @@ void update_fsm(uint32_t pc, int fsm_table_index, int history_index, bool taken)
 
 void reset_fsm(int fsm_table_index)
 {
-	for (int j = 0; j < BTB_context.fsm_tables[fsm_table_index].fsm.size(); j++)
-		{
-			// init fsms state
-			BTB_context.fsm_tables[fsm_table_index].fsm[j] = BTB_context.fsmState;
-		}
+	for (size_t j = 0; j < BTB_context.fsm_tables[fsm_table_index].fsm.size(); j++)
+	{
+		// init fsms state
+		BTB_context.fsm_tables[fsm_table_index].fsm[j] = BTB_context.fsmState;
+	}
 }
 
 void reset_history(int history_index)
@@ -138,11 +136,12 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned f
 	if ((btbSize != 1 && btbSize != 2 && btbSize != 4 &&
 		 btbSize != 8 && btbSize != 16 && btbSize != 32) ||
 		(historySize < 1 || historySize > 8) ||
-		(tagSize < 0 || tagSize > (30 - (int)log2(BTB_context.btbSize))) ||
 		(fsmState < 0 || fsmState > 3))
 	{
 		return -1;
 	}
+	if (tagSize < 0 || tagSize > (30 - (unsigned)log2(BTB_context.btbSize)))
+		return -1;
 
 	// init inputs:
 	BTB_context.btbSize = btbSize;		   // number of entries in BTB
@@ -157,11 +156,11 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned f
 	{
 		BTB_context.BTB.resize(btbSize);
 		BTB_context.history.resize(isGlobalHist ? 1 : btbSize);
-		for (int i = 0; i < BTB_context.history.size(); i++)
+		for (size_t i = 0; i < BTB_context.history.size(); i++)
 		{
 			reset_history(i);
 		}
-		for (int i = 0; i < BTB_context.BTB.size(); i++)
+		for (size_t i = 0; i < BTB_context.BTB.size(); i++)
 		{
 			BTB_context.BTB[i].valid = 0; // all lines are empty
 			// if global history: all BTB entries go to the same history_index
@@ -171,7 +170,7 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned f
 		}
 
 		BTB_context.fsm_tables.resize(isGlobalTable ? 1 : btbSize);
-		for (int i = 0; i < BTB_context.fsm_tables.size(); i++)
+		for (size_t i = 0; i < BTB_context.fsm_tables.size(); i++)
 		{
 			// size of each fsm table: 2^history_size
 			BTB_context.fsm_tables[i].fsm.resize(BTB_context.tableSize);
@@ -199,7 +198,7 @@ bool BP_predict(uint32_t pc, uint32_t *dst)
 		unsigned predicted_state;
 		int hist_ind = BTB_context.BTB[BTB_entry].history_index;
 		int fsm_tab_ind = BTB_context.BTB[BTB_entry].fsm_table_index;
-		int fsm_ind = fsm_index(pc,hist_ind);
+		int fsm_ind = fsm_index(pc, hist_ind);
 		predicted_state = BTB_context.fsm_tables[fsm_tab_ind].fsm[fsm_ind];
 		if (predicted_state == 2 || predicted_state == 3)
 		{ // taken
@@ -222,8 +221,9 @@ void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst)
 {
 	// statistics update
 	BTB_context.cmds_counter++;
-	if (targetPc != pred_dst) BTB_context.flushes_counter++; // mispredicted
-	
+	if ((taken && targetPc != pred_dst) || (!taken && pc + 4 != pred_dst))
+		BTB_context.flushes_counter++; // mispredicted
+
 	int btb_index = get_btb_index(pc);
 	if (BTB_context.BTB[btb_index].valid == 0)
 	{
@@ -240,17 +240,19 @@ void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst)
 		{
 			// command found: update fsm, history, target, and check for flush
 			BTB_context.BTB[btb_index].target = targetPc;
-			update_fsm(pc,BTB_context.BTB[btb_index].fsm_table_index, BTB_context.BTB[btb_index].history_index, taken);
+			update_fsm(pc, BTB_context.BTB[btb_index].fsm_table_index, BTB_context.BTB[btb_index].history_index, taken);
 			add_to_history(BTB_context.BTB[btb_index].history_index, taken); // make sure to update fsm first!
-		} 
+		}
 		else
 		{
 			// line full, different tag: replace line, reset and update fsm and history
 			BTB_context.BTB[btb_index].tag = get_relevant_tag(pc);
 			BTB_context.BTB[btb_index].target = targetPc;
 			// reset fsm and history if local
-			if (BTB_context.isGlobalTable == 0) reset_fsm(BTB_context.BTB[btb_index].fsm_table_index);
-			if (BTB_context.isGlobalHist == 0) reset_history(BTB_context.BTB[btb_index].history_index);
+			if (BTB_context.isGlobalTable == 0)
+				reset_fsm(BTB_context.BTB[btb_index].fsm_table_index);
+			if (BTB_context.isGlobalHist == 0)
+				reset_history(BTB_context.BTB[btb_index].history_index);
 			update_fsm(pc, BTB_context.BTB[btb_index].fsm_table_index, BTB_context.BTB[btb_index].history_index, taken);
 			add_to_history(BTB_context.BTB[btb_index].history_index, taken); // make sure to update fsm first!
 		}
@@ -272,7 +274,7 @@ void BP_GetStats(SIM_stats *curStats)
 	// clear memory:
 	BTB_context.BTB.clear();
 	BTB_context.history.clear();
-	for (int i = 0; i < BTB_context.fsm_tables.size(); i++)
+	for (size_t i = 0; i < BTB_context.fsm_tables.size(); i++)
 	{
 		BTB_context.fsm_tables[i].fsm.clear();
 	}
