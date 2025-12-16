5c86ce68d6f28390d0426e78dd7571757fd74da1
amir <amirgrossman@gmail.com>
2025-12-16 15:19:52 +0200

    add HW2 base files

---
+++ CompArch-hw2/cache.h
@@ -0,0 +1,71 @@
+// ############ IMPORTS ############
+#include <vector>
+
+using namespace std;
+
+// ############ Structs ############
+
+struct block {
+    unsigned tag;
+    bool valid;
+    bool dirty;
+    unsigned lastUsed;
+};
+
+typedef vector<block> way;
+
+struct cache{
+    vector<way> ways;
+    unsigned Size;
+    unsigned Assoc;
+    unsigned Cyc;
+};
+
+struct memConfig_t{
+    cache L1;
+    cache L2;
+    unsigned MemCyc;
+    unsigned BSize;
+    unsigned WrAlloc;
+    double L1ReadMissRate;
+    double L2ReadMissRate;
+    int numOperations;
+    double totalCycles;
+};
+
+// ############ Globals ############
+
+memConfig_t memConfig;
+
+// ############ Functions ############
+
+void memConfigInit(unsigned MemCyc, unsigned BSize, unsigned L1Size,
+              unsigned L2Size, unsigned L1Assoc, unsigned L2Assoc,
+              unsigned L1Cyc, unsigned L2Cyc, unsigned WrAlloc);
+
+block createBlock(unsigned tag);
+
+void destroyBlock(block &b);
+
+
+//////////////////////////////////////////////////
+
+// get to block location by address
+block getBlockLocationInCache(unsigned address, cache &c);
+
+// check if block matches address tag
+bool checkHit(unsigned address, block b);
+
+// write block to cache 
+void writeToCache(unsigned address, cache &c);
+
+// LRU
+block findBlockToReplace(unsigned address, cache &c);
+
+
+//////////////////////////////////////////////////
+
+
+void read(unsigned address); // return tot # of cycles, main will calc misses from that
+
+void write(unsigned address);
