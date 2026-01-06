0a5c21515eff65e224ba6c48cead065b6ebc56ec
amir <amirgrossman@gmail.com>
2026-01-06 15:04:05 +0200

    add planning and data structure

--- CompArch-hw3/dflow_calc.cpp
+++ CompArch-hw3/dflow_calc.cpp
@@ -2,8 +2,26 @@
 /* Implementation (skeleton)  for the dataflow statistics calculator */
 
 #include "dflow_calc.h"
+#include <vector>
+
+using namespace std;
+
+struct node{
+    int id;
+    int latency;
+    int dep_1_id;
+    int dep_2_id;
+    int depth; //?
+};
+
+vector<node> graph;
 
 ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[], unsigned int numOfInsts) {
+    int regs_in_use[32] = {-1};
+    // resize graph
+    // dependencies by src
+    // update depth by max{dependency depths} + opcode latency
+
     return PROG_CTX_NULL;
 }
 
@@ -11,14 +29,16 @@ void freeProgCtx(ProgCtx ctx) {
 }
 
 int getInstDepth(ProgCtx ctx, unsigned int theInst) {
-    return -1;
+    return graph[theInst].depth;
 }
 
 int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) {
+    
     return -1;
 }
 
 int getProgDepth(ProgCtx ctx) {
+    // find max depth in linear pass on graph vector
     return 0;
 }
 
