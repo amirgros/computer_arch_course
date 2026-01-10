bdfa5d4e8fd0e980ca4a643fba7d67edc7b8a4d9
amir <amirgrossman@gmail.com>
2026-01-10 20:05:32 +0200

    write non-init functions, and  memory allocation and deallocation

--- CompArch-hw3/dflow_calc.cpp
+++ CompArch-hw3/dflow_calc.cpp
@@ -11,35 +11,56 @@ struct node{
     int latency;
     int dep_1_id;
     int dep_2_id;
-    int depth; //?
+    int depth;
 };
 
-vector<node> graph;
+typedef vector<node> t_graph;
 
 ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[], unsigned int numOfInsts) {
-    int regs_in_use[32] = {-1};
-    // resize graph
+    // allocate graph dynamically so the returned ProgCtx remains valid
+    t_graph* graph_ptr = new vector<node>(numOfInsts);
+
+    int regs_in_use[32];
+    for (int i = 0; i < 32; ++i) regs_in_use[i] = -1;
+    // resize graph (already sized via constructor)
     // dependencies by src
     // update depth by max{dependency depths} + opcode latency
 
-    return PROG_CTX_NULL;
+    return (ProgCtx)(graph_ptr);
 }
 
 void freeProgCtx(ProgCtx ctx) {
+    if (ctx == PROG_CTX_NULL) return;
+    t_graph* graph = (t_graph*)ctx;
+    delete graph;
 }
 
 int getInstDepth(ProgCtx ctx, unsigned int theInst) {
+    t_graph graph = *(t_graph*)ctx;
     return graph[theInst].depth;
 }
 
 int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) {
-    
-    return -1;
+    t_graph graph = *(t_graph*)ctx;
+    if(theInst >= graph.size()) {
+        return -1;
+    }
+    node inst = graph[theInst];
+    *src1DepInst = inst.dep_1_id;
+    *src2DepInst = inst.dep_2_id;
+    return 0;
 }
 
 int getProgDepth(ProgCtx ctx) {
     // find max depth in linear pass on graph vector
-    return 0;
+    t_graph graph = *(t_graph*)ctx;
+    int max_depth = 0;
+    for(int i = 0; i < graph.size(); i++) {
+        if(graph[i].depth > max_depth) {
+            max_depth = graph[i].depth;
+        }
+    }
+    return max_depth;
 }
 
 
