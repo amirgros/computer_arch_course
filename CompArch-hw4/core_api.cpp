e143467db12c6ea38914951b8329b8234afc5a67
Aya Israeli <ayaisraeli12@gmail.com>
2026-01-23 09:27:06 +0200

    implemented blocked mt

---
+++ CompArch-hw4/core_api.cpp
@@ -0,0 +1,172 @@
+/* 046267 Computer Architecture - HW #4 */
+
+#include "core_api.h"
+#include "sim_api.h"
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+// threads context
+typedef struct
+{
+	int regs[8];
+	int pc;			// threads command line. check if line num or addr (aligned to 4 bytes)
+	int when_ready; // cycle when the thread will be ready
+	bool is_waiting;
+	bool finished;
+} thread;
+
+// cpu context
+typedef struct
+{
+	vector<thread> threads;
+	int num_threads;
+	int current_thread;
+	int threads_left;
+	int num_cycles; // for CPI
+	int count_inst; // for CPI
+} cpu;
+
+cpu b_cpu;
+cpu fg_cpu;
+
+void CORE_BlockedMT()
+{
+	// init cpu and threads
+	b_cpu.num_threads = SIM_GetThreadsNum();
+	b_cpu.current_thread = 0;
+	b_cpu.threads_left = b_cpu.num_threads;
+	b_cpu.num_cycles = 0;
+	b_cpu.threads.resize(b_cpu.num_threads);
+	for (int i = 0; i < b_cpu.threads.size(); i++)
+	{
+		for (int j = 0; j < REGS_COUNT; j++)
+			b_cpu.threads[i].regs[j] = 0;
+		b_cpu.threads[i].pc = 0;
+		b_cpu.threads[i].when_ready = 0;
+		b_cpu.threads[i].is_waiting = 0;
+		b_cpu.threads[i].finished = 0;
+	}
+
+	while (b_cpu.threads_left > 0)
+	{
+		// choose thread:
+		thread *curr_thread = &b_cpu.threads[b_cpu.current_thread];
+		if (curr_thread->finished || curr_thread->when_ready > b_cpu.num_cycles)
+		{
+			// find next ready thread:
+			bool found = false;
+			for (int i = 1; i < b_cpu.num_threads && !found; i++)
+			{
+				int curr_num = (i + b_cpu.current_thread) % b_cpu.num_threads;
+				if (!b_cpu.threads[curr_num].finished && b_cpu.threads[curr_num].when_ready <= b_cpu.num_cycles)
+				{
+					curr_thread = &b_cpu.threads[curr_num];
+					b_cpu.current_thread = curr_num;
+					b_cpu.num_cycles += SIM_GetSwitchCycles();
+					found = true;
+				}
+			}
+			if (!found) // idle - must wait
+			{
+				b_cpu.num_cycles++;
+				continue;
+			}
+		}
+
+		// execute:
+		b_cpu.count_inst++;
+		b_cpu.num_cycles++;
+		Instruction curr_inst;
+		SIM_MemInstRead(curr_thread->pc, &curr_inst, b_cpu.current_thread);
+		int src1_val = curr_thread->regs[curr_inst.src1_index];
+		int src2_val = (curr_inst.isSrc2Imm) ? curr_inst.src2_index_imm : curr_thread->regs[curr_inst.src2_index_imm];
+		int latency = 0;
+		switch (curr_inst.opcode)
+		{
+		case CMD_NOP:
+			break;
+		case CMD_ADD:
+		case CMD_ADDI:
+			curr_thread->regs[curr_inst.dst_index] = src1_val + src2_val;
+			break;
+		case CMD_SUB:
+		case CMD_SUBI:
+			curr_thread->regs[curr_inst.dst_index] = src1_val - src2_val;
+			break;
+		case CMD_LOAD:
+		{
+			uint32_t addr = src1_val + src2_val;
+			int32_t val;
+			SIM_MemDataRead(addr, &val);
+			curr_thread->regs[curr_inst.dst_index] = val;
+			latency = SIM_GetLoadLat();
+			curr_thread->is_waiting = true;
+			curr_thread->when_ready = b_cpu.num_cycles + latency; // add plus 1?
+			break;
+		}
+		case CMD_STORE:
+		{
+			uint32_t addr = curr_thread->regs[curr_inst.dst_index] + src2_val;
+			SIM_MemDataWrite(addr, src1_val);
+			latency = SIM_GetStoreLat();
+			curr_thread->is_waiting = true;
+			curr_thread->when_ready = b_cpu.num_cycles + latency;
+			break;
+		}
+		case CMD_HALT:
+			curr_thread->finished = true;
+			b_cpu.threads_left--;
+			break;
+		default:
+			break;
+		}
+		curr_thread->pc++; // I think starts from 0
+	}
+
+	// while threads not finished
+	// choose thread - current/ready
+	// execute
+	// check opcode
+	// update regs - arithmetics / load store
+	// update pc
+	// update thread context - if opcode have latency
+	// free structs and memory
+}
+
+void CORE_FinegrainedMT()
+{
+	// init mem
+	// init cpu and threads
+	// while threads not finished
+	// automatically switch to next thread
+	// execute
+	// check opcode
+	// update regs - arithmetics / load store
+	// update pc
+	// update thread context - if opcode have latency
+	// free structs and memory
+}
+
+double CORE_BlockedMT_CPI()
+{
+	return (double)b_cpu.num_cycles / b_cpu.count_inst;
+}
+
+double CORE_FinegrainedMT_CPI()
+{
+	return 0;
+}
+
+void CORE_BlockedMT_CTX(tcontext *context, int threadid)
+{
+	for (int i = 0; i < REGS_COUNT; i++)
+	{
+		context[threadid].reg[i] = b_cpu.threads[threadid].regs[i];
+	}
+}
+
+void CORE_FinegrainedMT_CTX(tcontext *context, int threadid)
+{
+}
